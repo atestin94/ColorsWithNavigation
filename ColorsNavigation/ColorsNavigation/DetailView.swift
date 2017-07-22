b1f914555d88d00eeb557e598353c0820a8e030b
Alex Testin <alextestin@Alexs-MacBook-Pro.local>
2017-07-21 23:41:05 -0500

    Could not figure out detail view

---
+++ ColorsNavigation/ColorsNavigation/DetailView.swift
@@ -0,0 +1,12 @@
+//
+//  DetailView.swift
+//  ColorsNavigation
+//
+//  Created by Alex Testin on 7/21/17.
+//  Copyright © 2017 Alex Testin. All rights reserved.
+//
+
+import Foundation
+class DetailView: ColorsNavController {
+    
+}
